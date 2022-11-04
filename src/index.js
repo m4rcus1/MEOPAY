4f948942f70b5066641d4ba85cef177e80d8a908
Nhat Loi <nhatloi120402@gmail.com>
2022-11-04 19:03:04 +0700

    Layout Web Task

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const hbs = require('express-handlebars')
+const path = require('path')
+const port = 3000
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.use(express.static('src'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`
+            Example app listening on http://localhost:${port}
+            `)
+})
