c69f9ce1bd1c80cd9a5f1f66449f4e169fb6bd32
Nhat Loi <nhatloi120402@gmail.com>
2022-11-08 03:07:47 +0700

    Add Register Page

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
 app.listen(port, () => {
     console.log(`
             Example app listening on http://localhost:${port}
