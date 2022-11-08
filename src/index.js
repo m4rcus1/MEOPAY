b3f27dd70164c0b84ea31e6e4fe7f35a58555f6d
Nhat Loi <nhatloi120402@gmail.com>
2022-11-08 13:49:52 +0700

    Add login first time page, update code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const hbs = require('express-handlebars')
 const path = require('path')
 const port = 3000
+const usersRoute = require('./resource/routes/users.route')
 
 app.engine('hbs', hbs.engine({
     extname: 'hbs'
@@ -12,17 +13,26 @@ app.set('views', path.join(__dirname, 'resource/views'));
 
 app.use(express.static('src'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
+// app.get('/login', (req, res) => {
+//     res.render('login')
+// })
+
+// app.get('/register', (req, res) => {
+//     res.render('register')
+// })
+
+// app.get('/login1st', (req, res) => {
+//     res.render('login1st')
+// })
+
+app.use('/', usersRoute);
 
 app.listen(port, () => {
     console.log(`
